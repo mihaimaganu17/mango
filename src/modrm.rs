0d3211b0e473aea1fcb7773e363b859a64e7dfc3
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2023-04-27 09:44:20 +0200

    Finished implementing addressing

--- src/modrm.rs
+++ src/modrm.rs
@@ -80,19 +80,19 @@ impl From<u8> for EffAddr16Bit {
                     // EAX/AX/AL/MM0/XMM0
                     0b000 => Self(Some(Reg::EAX), None, None),
                     // ECX/CX/CL/MM1/XMM1
-                    0b000 => Self(Some(Reg::ECX), None, None),
+                    0b001 => Self(Some(Reg::ECX), None, None),
                     // EDX/DX/DL/MM2/XMM2
-                    0b000 => Self(Some(Reg::EDX), None, None),
+                    0b010 => Self(Some(Reg::EDX), None, None),
                     // EBX/BX/BL/MM3/XMM3
-                    0b000 => Self(Some(Reg::EBX), None, None),
+                    0b011 => Self(Some(Reg::EBX), None, None),
                     // ESP/SP/AHMM4/XMM4
-                    0b000 => Self(Some(Reg::ESP), None, None),
+                    0b100 => Self(Some(Reg::ESP), None, None),
                     // EBP/BP/CH/MM5/XMM5
-                    0b000 => Self(Some(Reg::EBP), None, None),
+                    0b101 => Self(Some(Reg::EBP), None, None),
                     // ESI/SI/DH/MM6/XMM6
-                    0b000 => Self(Some(Reg::ESI), None, None),
+                    0b110 => Self(Some(Reg::ESI), None, None),
                     // EDI/DI/BH/MM7/XMM7
-                    0b000 => Self(Some(Reg::EDI), None, None),
+                    0b111 => Self(Some(Reg::EDI), None, None),
                     // Since we know only the low 3 bits can have a value in R/M, this option is
                     // only needed by the Rust compiler and something very wrong happened
                     _ => unreachable!(),
@@ -181,19 +181,19 @@ impl From<u8> for EffAddr32Bit {
                     // EAX/AX/AL/MM0/XMM0
                     0b000 => Self(EffAddrType::Reg(Reg::EAX), None),
                     // ECX/CX/CL/MM1/XMM1
-                    0b000 => Self(EffAddrType::Reg(Reg::ECX), None),
+                    0b001 => Self(EffAddrType::Reg(Reg::ECX), None),
                     // EDX/DX/DL/MM2/XMM2
-                    0b000 => Self(EffAddrType::Reg(Reg::EDX), None),
+                    0b010 => Self(EffAddrType::Reg(Reg::EDX), None),
                     // EBX/BX/BL/MM3/XMM3
-                    0b000 => Self(EffAddrType::Reg(Reg::EBX), None),
+                    0b011 => Self(EffAddrType::Reg(Reg::EBX), None),
                     // ESP/SP/AHMM4/XMM4
-                    0b000 => Self(EffAddrType::Reg(Reg::ESP), None),
+                    0b100 => Self(EffAddrType::Reg(Reg::ESP), None),
                     // EBP/BP/CH/MM5/XMM5
-                    0b000 => Self(EffAddrType::Reg(Reg::EBP), None),
+                    0b101 => Self(EffAddrType::Reg(Reg::EBP), None),
                     // ESI/SI/DH/MM6/XMM6
-                    0b000 => Self(EffAddrType::Reg(Reg::ESI), None),
+                    0b110 => Self(EffAddrType::Reg(Reg::ESI), None),
                     // EDI/DI/BH/MM7/XMM7
-                    0b000 => Self(EffAddrType::Reg(Reg::EDI), None),
+                    0b111 => Self(EffAddrType::Reg(Reg::EDI), None),
                     // Since we know only the low 3 bits can have a value in R/M, this option is
                     // only needed by the Rust compiler and something very wrong happened
                     _ => unreachable!(),
@@ -222,7 +222,63 @@ pub struct Addressing16Bit {
 /// base-plus-index and scale-plus-index forms of 32-bit addressing require the SIB byte.
 pub struct SIB(u8);
 
-impl From<u8> for SIB {
+// This represents the top 2 bits(Scale parameter) of the SIB byte in an x86_64 instruction
+pub struct Scale(u8);
+pub struct ScaledIndex(Option<Reg>, Option<Scale>);
+
+impl From<u8> for ScaledIndex {
     fn from(value: u8) -> Self {
+        let scale = (value >> 6) & 0b11;
+        let idx = (value >> 3) & 0b111;
+
+        let scaled_index = match scale {
+            0b00 => match idx {
+                0b000 => Self(Some(Reg::EAX), None),
+                0b001 => Self(Some(Reg::ECX), None),
+                0b010 => Self(Some(Reg::EDX), None),
+                0b011 => Self(Some(Reg::EBX), None),
+                0b100 => Self(Some(None, None),
+                0b101 => Self(Some(Reg::EBP), None),
+                0b110 => Self(Some(Reg::ESI), None),
+                0b111 => Self(Some(Reg::EDI), None),
+                _ => unreachable!(),
+            }
+            0b01 => match idx {
+                0b000 => Self(Some(Reg::EAX), Some(Scale(2))),
+                0b001 => Self(Some(Reg::ECX), Some(Scale(2))),
+                0b010 => Self(Some(Reg::EDX), Some(Scale(2))),
+                0b011 => Self(Some(Reg::EBX), Some(Scale(2))),
+                0b100 => Self(Some(None, None),
+                0b101 => Self(Some(Reg::EBP), Some(Scale(2))),
+                0b110 => Self(Some(Reg::ESI), Some(Scale(2))),
+                0b111 => Self(Some(Reg::EDI), Some(Scale(2))),
+                _ => unreachable!(),
+            }
+            0b10 => match idx {
+                0b000 => Self(Some(Reg::EAX), Some(Scale(4))),
+                0b001 => Self(Some(Reg::ECX), Some(Scale(4))),
+                0b010 => Self(Some(Reg::EDX), Some(Scale(4))),
+                0b011 => Self(Some(Reg::EBX), Some(Scale(4))),
+                0b100 => Self(Some(None, None),
+                0b101 => Self(Some(Reg::EBP), Some(Scale(4))),
+                0b110 => Self(Some(Reg::ESI), Some(Scale(4))),
+                0b111 => Self(Some(Reg::EDI), Some(Scale(4))),
+                _ => unreachable!(),
+            }
+            0b11 => match idx {
+                0b000 => Self(Some(Reg::EAX), Some(Scale(8))),
+                0b001 => Self(Some(Reg::ECX), Some(Scale(8))),
+                0b010 => Self(Some(Reg::EDX), Some(Scale(8))),
+                0b011 => Self(Some(Reg::EBX), Some(Scale(8))),
+                0b100 => Self(Some(None, None),
+                0b101 => Self(Some(Reg::EBP), Some(Scale(8))),
+                0b110 => Self(Some(Reg::ESI), Some(Scale(8))),
+                0b111 => Self(Some(Reg::EDI), Some(Scale(8))),
+                _ => unreachable!(),
+            }
+            _ => unreachable!(),
+        };
+
+        scaled_index
     }
 }
